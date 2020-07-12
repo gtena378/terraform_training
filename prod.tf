f5eaa8a16e5913ca414d7e76d5d21fbb3f834d86
Bobi Tena <gtena3@yahoo.com>
2020-07-12 03:53:46 -0400

    Defined initial web security group

--- prod.tf
+++ prod.tf
@@ -7,7 +7,39 @@ provider "aws" {
   #   shared_credentials_file = "/home/osboxes/.aws/credentials"
 }
 
-resource "aws_s3_bucket" "tf-course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tena-tf-course-20200709"
   acl    = "private"
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports nbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
 }
