0358d9a06ca75242de1e81efb6153fa4875632fe
Bobi Tena <gtena3@yahoo.com>
2020-07-13 01:16:44 -0400

    Load balancer for prod web

--- prod.tf
+++ prod.tf
@@ -10,9 +10,26 @@ provider "aws" {
 resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tena-tf-course-20200709"
   acl    = "private"
+  tags = {
+    "Terraform" : "true"
+  }
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+  tags = {
+    "Terraform" : "true"
+  }
+}
 
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
@@ -65,6 +82,24 @@ resource "aws_eip_association" "prod_web" {
 }
 
 resource "aws_eip" "prod_web" {
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name = "prod-web"
+  instances = aws_instance.prod_web.*.id
+  subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
   tags = {
     "Terraform" : "true"
   }
