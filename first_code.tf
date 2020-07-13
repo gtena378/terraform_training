a42a5d16dd1a6931618b742c67cded97d00c0122
Bobi Tena <gtena3@yahoo.com>
2020-07-13 09:49:33 -0400

    Refactor to use asg

--- first_code.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
-resource "aws_s3_bucket" "tf_course" {
-  bucket = "tena-tf-course-20200709"
-  acl    = "private"
-}
--- prod.tf
+++ prod.tf
@@ -1,10 +1,6 @@
 provider "aws" {
   profile    = "default"
   region     = "us-west-2"
-  access_key = "AKIASYIXHAJYZ3XX4BVU"
-  secret_key = "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"
-
-  #   shared_credentials_file = "/home/osboxes/.aws/credentials"
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
@@ -61,35 +57,8 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami           = "ami-01ebd27ddd89835f4"
-  instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
 resource "aws_elb" "prod_web" {
   name = "prod-web"
-  instances = aws_instance.prod_web.*.id
   subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
@@ -103,4 +72,36 @@ resource "aws_elb" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-01ebd27ddd89835f4"
+  instance_type = "t2.micro"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones = ["us-west-2a","us-west-2b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key = "Terraform"
+    value = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
